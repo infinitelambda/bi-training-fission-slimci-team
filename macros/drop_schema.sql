d44933ad53971f0350aea051b04bc1b07a269521
Irina Veleva <irina.n.veleva@gmail.com>
2021-11-01 17:47:17 +0200

    Add a new workflow on merge

---
+++ macros/drop_schema.sql
@@ -0,0 +1,14 @@
+{% macro drop_schema(schema=var('CI_SCHEMA')) %}
+
+  {{ log('Statement to run:', info=True) }}
+
+  {% set drop_command -%}
+    drop schema {{ target.database }}.{{ schema }};
+  {%- endset %}
+
+  {% do log(drop_command, info=True) %}
+  {% do run_query(drop_command) %}
+
+  {{ log('Schema dropped successfully', info=True) }}
+
+{% endmacro %}
